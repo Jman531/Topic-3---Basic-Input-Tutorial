33f55a4058441b0cfe050b6abe3e91acb801b107
Jack Gallagher <339690067@GoTVDSB.ca>
2024-09-06 09:20:05 -0400

    Add project files.

---
+++ Topic 3 - Basic Input Tutorial/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_3___Basic_Input_Tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
