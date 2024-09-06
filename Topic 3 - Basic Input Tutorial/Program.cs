70a40276451c1e512a5ae977989c5d2fbac25cb8
Jack Gallagher <339690067@GoTVDSB.ca>
2024-09-06 09:34:15 -0400

    Tutorial Finished

--- Topic 3 - Basic Input Tutorial/Program.cs
+++ Topic 3 - Basic Input Tutorial/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string response;
+            int number;
+            double numberDecimal;
+
+            Console.WriteLine("Say hello!");
+            response = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine($"You said: {response}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Give me an integer: ");
+            if(Int32.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine();
+
+                Console.WriteLine("You gave me a " + number);
+            }
+            else
+            {
+                Console.WriteLine();
+
+                Console.WriteLine("You didn't give me a integer so the number will be set to " + number + "!");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Give me a number: ");
+            if (Double.TryParse(Console.ReadLine(), out numberDecimal))
+            {
+                Console.WriteLine();
+
+                Console.WriteLine("You gave me the number " + numberDecimal + "!");
+            }
+            else
+            {
+                Console.WriteLine();
+
+                Console.WriteLine("You didn't give me a number so the number will be set to " + numberDecimal + "!");
+            }
+
+            Console.ReadLine();
         }
     }
 }
